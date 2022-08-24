0133922ae88be2c627c48aafcc1490f118121688
Lukas <lukas.kesch@gmail.com>
2022-08-24 22:27:06 +0200

    cleanup

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
+-- Hello World
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Your Title", informativeText="Hello World"}):send()
   hs.alert.show("Hello World")
+  print(hs.inspect("Test"))
 end)
 
 -- Reloads config
@@ -14,7 +16,7 @@ hs.hotkey.bind({"alt", "shift"}, "Z", function()
   hs.spotify.playTrack("spotify:track:2usrT8QIbIk9y0NEtQwS4j")
 end)
 
--- Playes "Coding Mode" playlist on spotify
+-- Playes "Coding Mode" playlist on spotify (Note: The id can be obtained from the spotify share url)
 hs.hotkey.bind({"alt"}, "Z", function()
   hs.spotify.playTrack("spotify:playlist:37i9dQZF1DX5trt9i14X7j?si=805a4491da454409")
 end)
